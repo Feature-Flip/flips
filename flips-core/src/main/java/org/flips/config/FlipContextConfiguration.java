54e67fee0b549750c93b72b7790e2d1d0e171e7c
sarthak <msarthak@thoughtworks.com>
2017-08-23 00:05:34 +0530

    Sarthak: Renamed MAVEN modules - removed the word spring, removed the module flip-spring-aop, now aop is a part of core module

--- flips-core/src/main/java/org/flips/config/FlipContextConfiguration.java
+++ flips-core/src/main/java/org/flips/config/FlipContextConfiguration.java
@@ -2,8 +2,10 @@ package org.flips.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
+@EnableAspectJAutoProxy
 @ComponentScan(basePackages = "org.flips")
 public class FlipContextConfiguration {
 }
--- flips-core/src/main/java/org/flips/exception/FeatureNotEnabledException.java
+++ flips-core/src/main/java/org/flips/exception/FeatureNotEnabledException.java
@@ -1,7 +1,6 @@
 package org.flips.exception;
 
 public class FeatureNotEnabledException extends RuntimeException {
-
     public FeatureNotEnabledException(String message) {
         super(message);
     }
