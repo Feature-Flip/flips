e49e2ace02d80fdc698ebedcab5c7be4aace2741
sarthak <msarthak@thoughtworks.com>
2017-09-16 20:01:16 +0530

    Sarthak: Added -
    @Target({ElementType.METHOD})
    @Retention(RetentionPolicy.RUNTIME)
    @FlipOnOff(value = DayOfWeekFlipCondition.class)
    
    on FlipOnDaysOfWeek annotation

--- flips-core/src/main/java/org/flips/annotation/FlipOnDaysOfWeek.java
+++ flips-core/src/main/java/org/flips/annotation/FlipOnDaysOfWeek.java
@@ -1,7 +1,16 @@
 package org.flips.annotation;
 
+import org.flips.condition.DayOfWeekFlipCondition;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.time.DayOfWeek;
 
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@FlipOnOff(value = DayOfWeekFlipCondition.class)
 public @interface FlipOnDaysOfWeek {
 
     DayOfWeek[] daysOfWeek();
